deabd61558c0e50e84942ffc0526b3c0baf3fc80
hegyes310 <bence.hegyes@gmail.com>
2025-01-16 15:25:29 +0100

    Changed const with let

--- use_SC.js
+++ use_SC.js
@@ -1,21 +1,21 @@
 require('dotenv').config();
-const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
-const API_URL = process.env.API_URL;
+let CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+let API_URL = process.env.API_URL;
 
-const Web3 = require('web3');
-const fs = require('fs');
+let Web3 = require('web3');
+let fs = require('fs');
 
 // Get the participant ID from the command line
 var argv = require('minimist')(process.argv.slice(2));
 var subject_ID = argv['_'][0];
 
 // Load the contract ABI
-const web3 = new Web3(API_URL);
-const artifactsPath = 'artifacts/contracts/EEG_HC.sol/UsersInfo.json';
-const contractData = JSON.parse(fs.readFileSync(artifactsPath, 'utf8'));
-const usersInfoAbi = contractData.abi;
-const usersInfoAddress = CONTRACT_ADDRESS; // The address of your deployed UsersInfo contract
-const usersInfoContract = new web3.eth.Contract(usersInfoAbi, usersInfoAddress);
+let web3 = new Web3(API_URL);
+let artifactsPath = 'artifacts/contracts/EEG_HC.sol/UsersInfo.json';
+let contractData = JSON.parse(fs.readFileSync(artifactsPath, 'utf8'));
+let usersInfoAbi = contractData.abi;
+let usersInfoAddress = CONTRACT_ADDRESS; // The address of your deployed UsersInfo contract
+let usersInfoContract = new web3.eth.Contract(usersInfoAbi, usersInfoAddress);
 
 
 
